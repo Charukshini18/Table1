80b932635beba67d1351bea06e8c6b9c9d302c6a
Charukshini18 <100907026+Charukshini18@users.noreply.github.com>
2022-03-04 12:46:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Components/Test';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
